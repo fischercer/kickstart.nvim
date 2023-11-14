d522cce3a606a4d9a945c45719681a66840ae471
Hans Ronald Fischer <fischer@cerris.at>
2023-11-14 21:34:45 +0100

    adds linter

--- init.lua
+++ init.lua
@@ -232,6 +232,35 @@ require('lazy').setup({
     },
   },
 
+  {
+    -- linter support to enable more strict checking
+    'mfussenegger/nvim-lint',
+    events = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
+    config = function()
+      local lint = require 'lint'
+      -- Event to trigger linters
+      lint.linters_by_ft = {
+        markdown = { 'vale' },
+        python = { 'ruff', 'mypy' },
+        go = { 'golangcilint' },
+        sh = { 'shellcheck' },
+        yaml = { 'yamllint' },
+        -- Use the "*" filetype to run linters on all filetypes.
+        -- ['*'] = { 'global linter' },
+        -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+        -- ['_'] = { 'fallback linter' },
+      }
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
