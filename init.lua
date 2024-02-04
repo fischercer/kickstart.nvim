45c757bac9ec385243ac467079ba3a743d1a7434
Hans Ronald Fischer <fischer@cerris.at>
2024-02-04 20:35:26 +0100

    fixes preselection of completion

--- init.lua
+++ init.lua
@@ -755,7 +755,7 @@ cmp.setup {
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
+        --cmp.select_next_item()
       elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       else
